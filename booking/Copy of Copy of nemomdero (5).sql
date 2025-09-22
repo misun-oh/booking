dd633acf8a4012b4bceb90cbf85439174b5aefe2
fprh123 <rkddn9546@naver.com>
2025-09-22 14:25:30 +0900

    sql 업로드

--- booking/Copy of Copy of nemomdero (5).sql
+++ booking/Copy of Copy of nemomdero (5).sql
@@ -20,7 +20,7 @@ CREATE TABLE `instructor` (
 	`img`	VARCHAR(255)	NULL,
 	`word`	VARCHAR(300)	NULL,
 	`name`	VARCHAR(100)	NULL,
-	`file_id`	INTEGER	NULL
+	`file_id`	INT	NULL
 );
 
 CREATE TABLE `category` (
@@ -34,7 +34,6 @@ CREATE TABLE `category` (
 CREATE TABLE `enrollment` (
 	`enrollment_id`	BIGINT	NULL,
 	`course_id`	BIGINT	NULL,
-	`instructor_id`	BIGINT	NULL,
 	`user_id`	BIGINT	NULL,
 	`status`	ENUM('ongoing','completed','cancelled')	NULL	DEFAULT 'ongoing',
 	`enrolled_at`	TIMESTAMP	NULL
@@ -43,10 +42,14 @@ CREATE TABLE `enrollment` (
 CREATE TABLE `lesson` (
 	`lesson_id`	BIGINT	NULL,
 	`course_id`	BIGINT	NULL,
-	`instructor_id`	BIGINT	NULL,
 	`lesson_name`	VARCHAR(250)	NULL,
-	`lesson_order`	INT	NULL,
-	`video_url`	VARCHAR(255)	NULL
+	`lesson_order`	VARCHAR(250)	NULL,
+	`file_id`	INT	NULL,
+	`lesson_explan`	VARCHAR(500)	NULL,
+	`fix_time`	LocalDateTime	NULL,
+	`delete_time`	LocalDateTime	NULL,
+	`resgistration_time`	LocalDateTime	NULL,
+	`saved.parh`	VARCHAR	NULL
 );
 
 CREATE TABLE `course` (
@@ -59,7 +62,7 @@ CREATE TABLE `course` (
 	`price`	DECIMAL	NULL,
 	`level`	ENUM('beginner', 'intermediate', 'advanced')	NULL,
 	`img`	VARCHAR(255)	NULL,
-	`file_id`	INTEFER	NULL
+	`file_id`	INT	NULL
 );
 
 ALTER TABLE `posts` ADD CONSTRAINT `PK_POSTS` PRIMARY KEY (
@@ -81,14 +84,12 @@ ALTER TABLE `category` ADD CONSTRAINT `PK_CATEGORY` PRIMARY KEY (
 ALTER TABLE `enrollment` ADD CONSTRAINT `PK_ENROLLMENT` PRIMARY KEY (
 	`enrollment_id`,
 	`course_id`,
-	`instructor_id`,
 	`user_id`
 );
 
 ALTER TABLE `lesson` ADD CONSTRAINT `PK_LESSON` PRIMARY KEY (
 	`lesson_id`,
-	`course_id`,
-	`instructor_id`
+	`course_id`
 );
 
 ALTER TABLE `course` ADD CONSTRAINT `PK_COURSE` PRIMARY KEY (
@@ -104,13 +105,6 @@ REFERENCES `course` (
 	`course_id`
 );
 
-ALTER TABLE `enrollment` ADD CONSTRAINT `FK_course_TO_enrollment_2` FOREIGN KEY (
-	`instructor_id`
-)
-REFERENCES `course` (
-	`instructor_id`
-);
-
 ALTER TABLE `enrollment` ADD CONSTRAINT `FK_user_TO_enrollment_1` FOREIGN KEY (
 	`user_id`
 )
@@ -123,13 +117,6 @@ ALTER TABLE `lesson` ADD CONSTRAINT `FK_course_TO_lesson_1` FOREIGN KEY (
 )
 REFERENCES `course` (
 	`course_id`
-);
-
-ALTER TABLE `lesson` ADD CONSTRAINT `FK_course_TO_lesson_2` FOREIGN KEY (
-	`instructor_id`
-)
-REFERENCES `course` (
-	`instructor_id`
 );
 
 ALTER TABLE `course` ADD CONSTRAINT `FK_category_TO_course_1` FOREIGN KEY (
